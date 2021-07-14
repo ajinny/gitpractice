7ac0aad5fc4bc4dad00aa9c68eda62483d0d9dcd
Anju <anju_gadodia@hotmail.com>
2021-07-14 03:04:02 +0000

    made changes

--- hello.java
+++ hello.java
@@ -2,10 +2,10 @@ class Hello
 {
 public static void main(String args[])
 {
-<<<<<<< Updated upstream
+
 System.out.println("(made changes in remote.  )need to fetch from child hello 2");
-=======
-System.out.println("making changes in master,from child hello 2");
->>>>>>> Stashed changes
+
+
+
 }
-}
+
