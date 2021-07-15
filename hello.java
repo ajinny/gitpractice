631d72ba134214d835f6dadadb8ed9a0078c42e4
Anju <anju_gadodia@hotmail.com>
2021-07-15 01:39:42 +0000

    added files

--- hello.java
+++ hello.java
@@ -1,11 +1,10 @@
-class Hello
+class hello
 {
 public static void main(String args[])
 {
 
-System.out.println("(made changes in remote.  )need to fetch from child hello 2");
-
+System.out.println("made changes in remote.  need to fetch from child hello 2");
+}
 
 
 }
-
